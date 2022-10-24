0cfdd7ee59825467c7c82d4a58fc2c43d98a85c5
Chrystian Silva <chrystian100@hotmail.com>
2022-10-24 01:35:21 -0300

    Added base authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,20 +1,66 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RoutesModule } from './routes/routes.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { join } from 'path';
-import { RouteSchema } from './@core/infra/db/typeorm/route.schema';
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { UsersModule } from './users/users.module';
+import { ForgotPasswordModule } from './forgot-password/forgot-password.module';
+import { ChangePasswordModule } from './change-password/change-password.module';
+import { MailerModule } from './mailer/mailer.module';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { UtilsModule } from './utils/utils.module';
+import * as Yup from 'yup';
+
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: join(__dirname, 'database.sqlite'),
-      synchronize: true,
-      logging: true,
-      entities: [RouteSchema],
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ['.env', '.env.dev', '.env.stage', '.env.prod'],
+      validationSchema: Yup.object({
+        TYPEORM_HOST: Yup.string().required(),
+        TYPEORM_PORT: Yup.number().default(3306),
+        TYPEORM_USERNAME: Yup.string().required(),
+        TYPEORM_PASSWORD: Yup.string().required(),
+        TYPEORM_DATABASE: Yup.string().required(),
+      }),
     }),
-    RoutesModule,
+    ThrottlerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        ttl: config.get<number>('THROTTLE_TTL'),
+        limit: config.get<number>('THROTTLE_LIMIT'),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'mysql',
+        host: config.get<string>('TYPEORM_HOST'),
+        port: config.get<number>('TYPEORM_PORT'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        synchronize: true,
+        entities: ['dist/**/*.entity.js'],
+        migrations: ['dist/migrations/**/*.js'],
+        subscribers: ['dist/subscriber/**/*.js'],
+        cli: {
+          migrationsDir: config.get<string>('TYPEORM_MIGRATIONS_DIR'),
+          subscribersDir: config.get<string>('TYPEORM_SUBSCRIBERS_DIR'),
+        },
+      }),
+    }),
+    LoginModule,
+    RegisterModule,
+    UsersModule,
+    ForgotPasswordModule,
+    ChangePasswordModule,
+    MailerModule,
+    UtilsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
