b88e8e5b86b911b8d252cc186967c6a822e4ac67
Carlos Guilherme Felismino Pedroni <calosguilherme@hotmail.com>
2022-11-30 01:11:31 -0300

    ajustes em algumas classes e começo do mock

--- frontend/src/app/model/registroMovimentacao.model.ts
+++ frontend/src/app/model/registroMovimentacao.model.ts
@@ -1,8 +1,10 @@
+import { Time } from "@angular/common";
 import { Entidade } from "./entidade.model";
 
 export class RegistroMovimentacao extends Entidade {
-    dataMovimentacao?: Date
-    localOrigem?: String
-    localDestino?: String
+    data?: Date;
+    hora?: Time;
+    local?: String;
+    subStatus?: [];
     status?: String
 }
