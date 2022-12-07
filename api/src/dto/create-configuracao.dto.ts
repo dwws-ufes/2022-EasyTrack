f3c94fa3fee58ff40c6df9c1b91e9f5d0f31c1ab
Carlos Guilherme Felismino Pedroni <calosguilherme@hotmail.com>
2022-12-07 00:37:34 -0300

    correcao commit anterior, exclusao de arquivos

--- api/src/dto/create-configuracao.dto.ts
+++
@@ -1,6 +0,0 @@
-export class CreateConfiguracaoDto {
-    update_automatico: boolean;
-    tipo_ordenacao_padrao: number;
-    intervalo_atualizacao: number;
-    horario_comercial_atualizacao: boolean;
-}
--- api/src/dto/update-configuracao.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateConfiguracaoDto } from './create-configuracao.dto';
-
-export class UpdateConfiguracaoDto extends PartialType(CreateConfiguracaoDto) {}
