096fe69867f0d898aaeacb32d4cd6922909ab5d4
roseliwenzel <roseliwen@gmail.com>
2022-12-07 21:27:43 -0300

    Delete front/src/app/model directory

--- front/src/app/model/configuracao.model.ts
+++
@@ -1,8 +0,0 @@
-import { Entidade } from "./entidade.model";
-
-export class Configuracao extends Entidade {
-    atualizacaoAutomatica: boolean = false
-    tipoOrdenacaoPadrao: number = 0
-    intervaloAtualizacao: number = 1
-    horarioComercialAtualizacao: boolean = false
-}
